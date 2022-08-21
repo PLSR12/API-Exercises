3c616d3fc1c44b2379c84dda80cef13807694fbc
Pedro Lucas Dos Santos <90657983+PLSR12@users.noreply.github.com>
2022-08-21 19:32:39 -0300

    [*] updated routes

--- src/routes.js
+++ src/routes.js
@@ -6,10 +6,10 @@ import multerConfig from './config/multer'
 const upload = multer(multerConfig)
 const routes = new Router()
 
-import SessionController from './app/controllers/SessionController'
-import UserController from './app/controllers/UserController'
 import ArticlesController from './app/controllers/ArticlesController'
 import CategoryController from './app/controllers/CategoryController'
+import SessionController from './app/controllers/SessionController'
+import UserController from './app/controllers/UserController'
 
 import authMiddleware from './app/middlewares/auth'
 
@@ -18,19 +18,17 @@ routes.get('/', (req, res) => {
 })
 
 routes.post('/users', UserController.store)
-
 routes.post('/sessions', SessionController.store)
+routes.get('/article', ArticlesController.index)
+routes.get('/article/:id', ArticlesController.show)
+routes.get('/category', CategoryController.index)
 
 routes.use(authMiddleware)
-
-routes.post('/articles', upload.single('file'), ArticlesController.store)
-routes.get('/articles', ArticlesController.index)
-routes.get('/article/:id', ArticlesController.show)
-routes.put('/articles/:id', upload.single('file'), ArticlesController.update)
+routes.post('/article', upload.single('file'), ArticlesController.store)
+routes.put('/article/:id', upload.single('file'), ArticlesController.update)
 routes.delete('/article/:id', ArticlesController.delete)
 
 routes.post('/category', upload.single('file'), CategoryController.store)
-routes.get('/category', CategoryController.index)
 routes.put('/category/:id', upload.single('file'), CategoryController.update)
 routes.delete('/category/:id', CategoryController.delete)
 
